b2b5297534d3ad2e1cac4123ee3f2a46a4026167
ants <ants293@gmail.com>
2020-10-25 19:23:01 +0200

    ArticleHeader styles and links.

--- src/components/article/ArticleHeader/ArticleHeader.tsx
+++ src/components/article/ArticleHeader/ArticleHeader.tsx
@@ -1,4 +1,10 @@
 import React, { ReactElement } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faExternalLinkAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import Header from "../../shared/Header/Header";
 import HeaderInnerContent from "../../shared/Header/HeaderInnerContent/HeaderInnerContent";
 
@@ -8,12 +14,22 @@ interface Props {
 }
 
 export default function ArticleHeader({ loading, data }: Props): ReactElement {
+  const { newsItem } = data;
+
   return (
     <>
       {!loading ? (
         <Header>
           <HeaderInnerContent>
-            <div className="header__title">The title</div>
+            <div className="header__inner">
+              <Link to="/">
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </Link>
+              <div className="header__title">The title</div>
+              <a href={newsItem.url} target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </div>
           </HeaderInnerContent>
         </Header>
       ) : (
