4554dc555c62e158fe1ac2523a90dcf41780c373
ants <ants293@gmail.com>
2020-10-25 23:12:21 +0200

    Added react slide animation.

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,28 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
 import React, { ReactElement } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ArticlesView from "../components/views/Articles/Articles.view";
 import ArticleView from "../components/views/Articles/Article.view";
+
+const AnimatedSwitch = withRouter(({ location }) => (
+  <TransitionGroup>
+    <CSSTransition
+      key={location.key}
+      classNames="slide-righttoleft"
+      timeout={500}
+    >
+      <Switch location={location}>
+        <Route exact path="/" component={ArticlesView} />
+        <Route path="/article/:id" component={ArticleView} />
+      </Switch>
+    </CSSTransition>
+  </TransitionGroup>
+));
 
 export default function RootRouter(): ReactElement {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ArticlesView} />
-        <Route path="/article/:id" component={ArticleView} />
-      </Switch>
+      <AnimatedSwitch />
     </BrowserRouter>
   );
 }
