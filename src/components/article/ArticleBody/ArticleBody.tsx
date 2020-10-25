94c16b8a1e772290bd6c7800a1f14fccb9a64c50
ants <ants293@gmail.com>
2020-10-25 18:52:35 +0200

    Single article api data html.

--- src/components/article/ArticleBody/ArticleBody.tsx
+++ src/components/article/ArticleBody/ArticleBody.tsx
@@ -1,5 +1,32 @@
 import React, { ReactElement } from "react";
 
-export default function ArticleBody(): ReactElement {
-  return <div className="single-article">yolo</div>;
+interface Props {
+  loading?: boolean;
+  data?: any;
 }
+
+export default function ArticleBody({ loading, data }: Props): ReactElement {
+  const { newsItem } = data;
+  return (
+    <div className="single-article">
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <>
+          <div className="single-article__title">{newsItem.title}</div>
+          <div className="single-article__img">
+            <img src={newsItem.img} alt={newsItem.title} />
+          </div>
+          <div className="single-article__content">{newsItem.content}</div>
+        </>
+      )}
+    </div>
+  );
+}
+
+ArticleBody.defaultProps = {
+  loading: false,
+  data: {
+    newsItem: {},
+  },
+};
--- src/components/views/Articles/Article.view.tsx
+++ src/components/views/Articles/Article.view.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import Wrapper from "../../shared/Wrapper/Wrapper";
 import { GET_SINGLE_ARTICLE_INFO } from "../../../apollo/requests/article.requests";
+import ArticleBody from "../../article/ArticleBody/ArticleBody";
 
 interface MatchParams {
   id: string;
@@ -11,18 +12,19 @@ interface MatchParams {
 type MatchProps = RouteComponentProps<MatchParams>;
 
 export default function ArticleView({ match }: MatchProps): ReactElement {
-  const { loading, error, data, fetchMore } = useQuery(
-    GET_SINGLE_ARTICLE_INFO,
-    {
-      variables: {
-        id: match.params.id,
-      },
-    }
-  );
+  const { loading, error, data } = useQuery(GET_SINGLE_ARTICLE_INFO, {
+    variables: {
+      id: match.params.id,
+    },
+  });
+
+  if (error) {
+    console.error(error);
+  }
 
   return (
     <Wrapper>
-      <div>single article</div>
+      <ArticleBody loading={loading} data={data} />
     </Wrapper>
   );
 }
